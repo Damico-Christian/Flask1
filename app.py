897811a0655506e6f2728726b2868db10175f5b9
Damico-Christian <159425303+Damico-Christian@users.noreply.github.com>
2025-12-02 07:43:10 +0000

    pymysql

--- app.py
+++ app.py
@@ -1,13 +1,33 @@
-from flask import Flask, jsonify # type: ignore
-from flask_cors import CORS  # type: ignore
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+from database_wrapper import DatabaseWrapper
+
 app = Flask(__name__)
-CORS(app) #abilita CORS per TUTTE le route
-@app.route("/profilo")
-def profilo():
-    return jsonify({
-    "nome": "Christian",
-    "professione": "Studente",
-    "status": "swag"
-})
-if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+CORS(app)
+
+#credenziali del db Aiven
+db = DatabaseWrapper(
+    host="mysql-1197a299-iisgalvanimi-2ff7.g.aivencloud.com",
+    user="avnadmin",
+    password="AVNS_EkGkeBVLvXC726rqo8_",
+    database="defaultdb",
+    port=22433  
+)
+
+
+#restituisce tutta la lista
+@app.route("/spesa", methods=["GET"])
+def get_spesa():
+    lista = db.get_lista_spesa()
+    return jsonify(lista)
+
+#aggiunge un elemento
+@app.route("/spesa", methods=["POST"])
+def aggiungi_spesa():
+    dati = request.json           #prende il JSON inviato da Angular
+    elemento = dati["elemento"]   #lo mettiamo in una variabile
+    db.aggiungi_elemento(elemento)
+    return jsonify({"msg": "Aggiunto"})
+    
+if __name__ == '__main__':
+    app.run(debug=True)
